94d593c7ec320c6cd4a3b24a8110aefce47cd8d4
Akstrov <yalam385.ya@gmail.com>
2023-03-22 23:49:47 +0000

    added documentations

--- Client.py
+++ Client.py
@@ -1,23 +1,45 @@
+# It creates a socket object, connects to the server, sends data to the server, receives data from the
+# server, and closes the connection to the server
 import socket
 import pickle
 
 
 class Client:
     def __init__(self, host='127.0.0.1', port=5000):
+        """
+        The function __init__() is a constructor that is called when an object of a class is
+        instantiated
+        
+        :param host: The IP address of the server, defaults to 127.0.0.1 (optional)
+        :param port: The port number that the server is listening on, defaults to 5000 (optional)
+        """
         self.host = host
         self.port = port
+        # Creating a socket object and connecting to the server.
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((self.host, self.port))
 
     def send_data(self, data):
+        """
+        It takes a string, converts it to bytes, and sends it to the server
+        
+        :param data: The data to send to the server
+        """
         self.client.sendall(bytes(data, "utf-8"))
 
     def receive_data(self):
+        """
+        It receives data from the server and unpickles it
+        :return: The data is being returned.
+        """
         data = self.client.recv(4096)
         data = pickle.loads(data)
         return data
 
     def close_connection(self):
+        """
+        It closes the connection to the server
+        """
         self.client.close()
 
 
--- Server.py
+++ Server.py
@@ -1,9 +1,18 @@
+# It creates a server that accepts connections and sends a dictionary to the client
 import socket
 import pickle
 
 
 class Server:
     def __init__(self, host='127.0.0.1', port=5000):
+        """
+        The function takes in two arguments, host and port, and sets them to the variables self.host and
+        self.port. Then, it creates a socket object and binds it to the host and port. Finally, it
+        listens for incoming connections
+
+        :param host: The IP address of the server, defaults to 127.0.0.1 (optional)
+        :param port: The port number that the server will listen on, defaults to 5000 (optional)
+        """
         self.host = host
         self.port = port
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,6 +20,10 @@ class Server:
         self.server.listen()
 
     def accept_connections(self):
+        """
+        It accepts a connection from a client, prints a message, sends a pickled dictionary to the
+        client, and closes the connection
+        """
         while True:
             client, address = self.server.accept()
             print(f"Connection from {str(address)} has been established.")
